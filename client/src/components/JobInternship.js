d8fd6cfcbcfd155f8bc4eb633d09f85b9e9363ed
ankitjena <ankitjena13@gmail.com>
2018-11-13 22:02:08 +0530

    jobs page

--- client/src/components/JobInternship.js
+++ client/src/components/JobInternship.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import StudentSideBar from './StudentSideBar'
+import { Grid, Card, Container } from 'semantic-ui-react';
+
+const items = [
+  {
+    header: 'Project Report - April',
+    description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+    meta: 'ROI: 30%',
+  },
+  {
+    header: 'Project Report - May',
+    description: 'Bring to the table win-win survival strategies to ensure proactive domination.',
+    meta: 'ROI: 34%',
+  },
+  {
+    header: 'Project Report - May',
+    description: 'Bring to the table win-win survival strategies to ensure proactive domination.',
+    meta: 'ROI: 34%',
+  },
+  {
+    header: 'Project Report - May',
+    description: 'Bring to the table win-win survival strategies to ensure proactive domination.',
+    meta: 'ROI: 34%',
+  },
+]
 
 const Jobs = () => {
   return (
-    <div>
-      <StudentSideBar curr="jobs" />
-      <div>Jobs here</div>
-    </div>
+    <Container>
+      <br />
+      <Grid columns={2} >
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <StudentSideBar curr="jobs" />
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <Card.Group centered items={items} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   )
 }
 
